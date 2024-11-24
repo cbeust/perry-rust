f603c6b7601836b133730d9bff6d4784805e2366
Cedric Beust <cedric@beust.com>
2024-11-24 10:21:31 -0800

    Fix HEAD

--- web/src/main.rs
+++ web/src/main.rs
@@ -77,8 +77,7 @@ async fn main() -> std::io::Result<()> {
             .service(resource("/favicon.{ext}").route(get().to(favicon)))
 
             // Cycles
-            .service(resource("/").route(get().to(index)))
-            .service(resource("/").route(head().to(root_head)))
+            .service(resource("/").route(get().to(index)).route(head().to(root_head)))
             .service(resource("/cycles/{number}").route(get().to(cycle)))
             .service(resource("/api/cycles/{number}").route(get().to(api_cycles)))
 
